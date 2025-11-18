bef423a606e3be13cd25a9cbbc72b8cfed72c439
jeremycheongzm <115978571+jeremycheongzm@users.noreply.github.com>
2025-11-18 09:25:35 +0000

    update vite.config.js

--- 5. Developing Front-End Apps with React/e-plantShopping/vite.config.js
+++ 5. Developing Front-End Apps with React/e-plantShopping/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/IBM-Full-Stack-Software-Developer/5. Developing Front-End Apps with React/e-plantShopping",
+    base: "/IBM-Full-Stack-Software-Developer/",
   plugins: [react()],
 })
